9f9e2e1a5449ad2ea6bbe4ae1d98a9ece77233af
epqlffltm <dsfake1225@gmail.com>
2025-11-24 10:12:25 +0900

    onoff_code

---
+++ code/on.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<pigpio.h>
+
+#define PIN 17
+#define on 1
+
+int main(void)
+{
+  if (gpioInitialise() < 0)
+  {
+    printf("pigpio 초기화 실패\n");
+    return 1;
+  }
+
+  int pin = PIN;
+
+  gpioSetMode(pin, PI_OUTPUT);
+
+  gpioWrite(pin,ON);
+
+  return 0;
+}
